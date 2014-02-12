d3c7772e2b07d8cf4c7102f90c2f42811fcb9716
tmont <tommy.mont@gmail.com>
2014-02-12 13:20:57 -0800

    inject key/config into redis configurator

--- src/web/configurators/redis.js
+++ src/web/configurators/redis.js
@@ -1,21 +1,10 @@
-module.exports = function(container, libs) {
-	var cfg = container.resolveSync('Config').redis,
-		log = container.resolveSync('Logger'),
-		redis = libs.redis,
-		lifetime = libs.sahara.lifetime;
+module.exports = function(key, redisConfig) {
+	return function(container, libs) {
+		var redis = libs.redis,
+			lifetime = libs.sahara.lifetime;
 
-	var client = redis.createClient({
-		host: cfg.host,
-		port: cfg.port
-	});
+		var client = redis.createClient(redisConfig);
 
-
-	// redis error handling is just fucking embarrassing:
-	// apparently this catches ALL errors, not just errors emitted by
-	// redis. WHAT THE FUCK.
-//	client.on('error', function(err) {
-//		log.error('Redis error', err);
-//	});
-
-	container.registerInstance(client, 'RedisClient', lifetime.memory());
+		container.registerInstance(client, key || 'RedisClient', lifetime.memory());
+	};
 };
