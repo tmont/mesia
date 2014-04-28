0ac4b4adf050b2ff5334ea04242e1a4abf7b1a48
tmont <tommy.mont@gmail.com>
2014-04-28 12:56:40 -0700

    upgraded jade

--- tests/jade-tests.js
+++ tests/jade-tests.js
@@ -11,6 +11,25 @@ describe('jade compiler', function() {
 		});
 	});
 
+	it('should compile template with case fall through', function(done) {
+		var compiler = new JadeCompiler();
+		var template = '\n\
+- var foo = 10;\n\
+case foo\n\
+	when 0\n\
+	when 1\n\
+		p you are a loser\n\
+	when 10\n\
+	default\n\
+		p Lots\n\
+';
+		compiler.compile(template, null, function(err, tmpl) {
+			should.not.exist(err);
+			tmpl({}).should.equal('<p>Lots</p>');
+			done();
+		});
+	});
+
 	it('should compile template for client', function(done) {
 		var compiler = new JadeCompiler();
 		compiler.compile('p Hello', { client: true }, function(err, tmpl) {
